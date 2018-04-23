880c29860fd2685b6cd3b18dc363139b248fd494
a0581308 <allan-herron@outlook.com>
2018-04-23 17:03:21 -0500

    New Commit Message

---
+++ TestProjectUI/TestProjectUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The following is used to show Hello World to the end-user.
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
